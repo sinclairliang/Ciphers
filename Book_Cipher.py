2d51ae2ecbae13eb7ce7822a231688c8072a4a8b
Sinclair Liang <sinclairliang@gmail.com>
2018-12-22 06:17:34 -0800

    encoding

--- Book_Cipher.py
+++ Book_Cipher.py
@@ -2,11 +2,13 @@ import re
 
 text = "Apple Banana Cherry Dog Elephant Ferry Goose Gray "
 
+
 def get_book_text(book_aadress):
     original_text = open(book_aadress).read()
     new_text = re.sub('[^a-zA-Z0-9]', r' ', original_text)
     re.sub(' +', ' ', new_text).strip()
     return new_text
+
 
 def get_words(text):
     list_of_words = list()
@@ -19,24 +21,34 @@ def get_words(text):
 
 def get_numbers(words):
     numbers = dict()
-    for i in range(0, len(words)-1):
+    for i in range(0, len(words) - 1):
         if len(words[i]) > 0:
             current_letter = words[i][0]
             if current_letter in numbers:
-                numbers[current_letter].append(i+1)
+                numbers[current_letter].append(i + 1)
             else:
                 numbers[current_letter] = list()
-                numbers[current_letter].append(i+1)
+                numbers[current_letter].append(i + 1)
     return numbers
 
 
+def encode(text, numbers):
+    output_list = list()
+    for i in range(0, len(text) - 1):
+        current_letter = text[i].upper()
+        if current_letter in numbers:
+            output_list.append(numbers[current_letter].pop(0))
+    return output_list
 
 
 def main():
     new_text = get_book_text('test.txt')
     words = get_words(new_text)
     numbers = get_numbers(words)
-    print(numbers)
+    message = "Hello"
+    l = encode(message, numbers)
+    print(l)
+
 
 
 main()
