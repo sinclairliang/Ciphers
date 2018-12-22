8516e0e2f79ca2e6f37bc65a39ca177fa5de8974
Sinclair Liang <sinclairliang@gmail.com>
2018-12-22 06:10:10 -0800

    transformed text to numbers

--- Book_Cipher.py
+++ Book_Cipher.py
@@ -1,17 +1,42 @@
 import re
 
-text = "We talk about social issues, political events and technological progress all the time. In other words, we wish and are witnessing our world to be a better one. Since there is a better world other than this one, it implies our world is not perfect. Is there really a more perfect world? What roles has perfection played in Descartes’ and Leibniz’s philosophical writings? Both philosophers considered God to be the “perfection.” If that is true, how do we reconcile God’s existence with issues we face daily? In this essay I attempt to analyze, examine and compared the notion of “Perfection” on both philosophers’ work. "
+text = "Apple Banana Cherry Dog Elephant Ferry Goose Gray "
 
+def get_book_text(book_aadress):
+    original_text = open(book_aadress).read()
+    new_text = re.sub('[^a-zA-Z0-9]', r' ', original_text)
+    re.sub(' +', ' ', new_text).strip()
+    return new_text
 
 def get_words(text):
-    text.replace(",", "")
-    text.replace(".", "")
     list_of_words = list()
-    words =  re.split(r'\s+', text)
+    words = re.split(r'\s+', text)
     # return list_of_words
     for word in words:
         list_of_words.append(word.upper())
     return list_of_words
 
 
+def get_numbers(words):
+    numbers = dict()
+    for i in range(0, len(words)-1):
+        if len(words[i]) > 0:
+            current_letter = words[i][0]
+            if current_letter in numbers:
+                numbers[current_letter].append(i+1)
+            else:
+                numbers[current_letter] = list()
+                numbers[current_letter].append(i+1)
+    return numbers
 
+
+
+
+def main():
+    new_text = get_book_text('test.txt')
+    words = get_words(new_text)
+    numbers = get_numbers(words)
+    print(numbers)
+
+
+main()
